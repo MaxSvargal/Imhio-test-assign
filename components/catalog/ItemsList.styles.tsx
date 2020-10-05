2219752f82fd76d87d5e321ada095235f1ad88ca
MaxSvargal <maxsvargal@gmail.com>
2020-10-05 13:03:34 +0700

    Switch to min width 270px of cards

--- components/catalog/ItemsList.styles.tsx
+++ components/catalog/ItemsList.styles.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const ItemsListBox = styled.ul`
   background: ${(props) => props.theme.foregroundColor};
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
   padding: 0;
   margin: 0;
   box-sizing: border-box;
